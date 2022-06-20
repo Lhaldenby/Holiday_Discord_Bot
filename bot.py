31af5335a624763ac61ef136ffaac7b28a566bbd
Lewis Haldenby <lhaldenby52@gmail.com>
2022-06-20 09:30:28 +0100

    Added an @ in the daily message

--- bot.py
+++ bot.py
@@ -126,7 +126,7 @@ async def send_daily_message():
 		today = datetime.date.today()
 		future = datetime.date(2023,10,14)
 		diff = future - today
-		await channelToSend.send(f'<@979099587560218644> Japan Countdown {diff.days} days remaining')
+		await channelToSend.send(f'<@&979099587560218644> Japan Countdown {diff.days} days remaining')
 
 @bot.event
 async def on_command_error(ctx, error):
