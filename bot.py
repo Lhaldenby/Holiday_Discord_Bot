0aaae97bfcfddcb419ba19afcb568887ee756ea1
Lewis Haldenby <lhaldenby52@gmail.com>
2025-02-27 17:33:23 +0000

    updated discord bot for japan 2026

--- bot.py
+++ bot.py
@@ -1,151 +1,141 @@
-from http import client
-import os, discord, datetime, asyncio, string
+import discord
+import asyncio
+import datetime
+import requests
+import gspread
+import json
+from discord.ext import commands
+from discord.ext.commands import cooldown, BucketType
+from oauth2client.service_account import ServiceAccountCredentials
 
-from dotenv import load_dotenv
-from discord.ext import tasks, commands
-from dataclasses import dataclass
-from translate import Translator
-from time import sleep
+# Load configuration from config.json
+with open("config.json", "r") as config_file:
+    config = json.load(config_file)
 
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
-GUILD = os.getenv('DISCORD_GUILD')
-CHANNEL = os.getenv('DISCORD_CHANNEL')
-intents = discord.Intents.all()
-bot = commands.Bot(command_prefix='!',intents=intents)
+TOKEN = config["DISCORD_BOT_TOKEN"]
+EXCHANGE_API_URL = config["EXCHANGE_API_URL"]
+CHANNEL_ID = config["CHANNEL_ID"]
+HEADERS = {
+    "X-RapidAPI-Key": config["RAPIDAPI_KEY"],
+    "X-RapidAPI-Host": config["RAPIDAPI_HOST"]
+}
 
-@dataclass
-class Person:
-	name: string
-	going: bool
-	timeoff: bool
-	flight: bool
-	lodging: bool
+# Google Sheets authentication
+SCOPE = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
+CREDS_FILE = config["GOOGLE_CREDS_FILE"]
+SPREADSHEET_NAME = config["SPREADSHEET_NAME"]
 
-Group = [
-	Person("Lewis Haldenby",True,False,False,False),
-	Person("Michael Bew",True,False,False,False),
-	Person("Adrian Larsson",False,False,False,False),
-	Person("Nebula Thibaut",True,True,False,False),
-	Person("Ryan Johnson",True,False,False,False),
-	Person("Jack Sweasey",True,False,False,False),
-	Person("Thomas Haldenby",True,False,False,False),
-	Person("Brie Mathieson",True,False,False,False),
-	Person("Ethan Hart-Coombes",True,False,False,False),
-	Person("Beth Middleton",True,False,False,False),
-	Person("Oli Ali",False,False,False,False)
-]
+def get_google_sheet_data():
+    try:
+        creds = ServiceAccountCredentials.from_json_keyfile_name(CREDS_FILE, SCOPE)
+        client = gspread.authorize(creds)
+        sheet = client.open(SPREADSHEET_NAME).get_worksheet(3)  # Select the first sheet
+        return sheet
+    except Exception as e:
+        print("Error fetching Google Sheet data:", e)
+        return None
 
-#DONE
-@bot.command(name='days', help='Responds with the days remaining until our trip')
-@commands.cooldown(1,15,commands.BucketType.channel)
-async def days_command(ctx):
-	today = datetime.date.today()
-	future = datetime.date(2023,10,14)
-	diff = future - today
-	if diff.days == 0:
-		await ctx.send("Today is the day!!")
-	elif diff.days < 0 and diff.days >= -14:
-		await ctx.send("We are in Japan, get off discord!")
-	elif diff.days < -14:
-		await ctx.send("We miss Japan :(")
-	else:
-		await ctx.send(f'{diff.days} days until our UWU Japan Trip')	
+intents = discord.Intents.default()
+intents.messages = True
+intents.guilds = True
+intents.message_content = True
+bot = commands.Bot(command_prefix="!", intents=intents)
 
-#DONE
-@bot.command(name='time', help='Responds with the local Japan time')
-@commands.cooldown(1,15,commands.BucketType.channel)
-async def time_command(ctx):
-	today = datetime.datetime.today() + datetime.timedelta(hours=9)
-	formatToday = today.strftime("%d/%m/%Y %H:%M:%S")
-	await ctx.send(f'Japan datetime: {formatToday}')
+def get_yen():
+    try:
+        querystring = {"to": "JPY", "from": "GBP", "q": "1.0"}
+        response = requests.get(EXCHANGE_API_URL, headers=HEADERS, params=querystring)
+        yen_rate = response.json()
+        if yen_rate:
+            return (f"Current exchange rate: 1 GBP = {yen_rate} JPY")
+        else:
+            return ("Could not fetch exchange rate.")
+    except Exception as e:
+        return ("Error fetching exchange rate {e}.")
 
-#DONE
-@bot.command(name='going_list', help='Whos going on the trip and booked what')
-@commands.cooldown(1,30,commands.BucketType.channel)
-async def going_command(ctx):
-	for person in Group:
-		going = "isn't going"
-		timeoff = "don't have time off"
-		flight = "don't have their flights booked"
-		lodging = "don't have a place to stay"
-		if person.timeoff:
-			timeoff = "have time off"
-		if person.flight:
-			flight = "have their flights booked"
-		if person.lodging:
-			lodging = "have a place to stay"
-		if person.going:
-			going = "is going"
-		await ctx.send(f'{person.name} {going}. They {timeoff}, {flight} and {lodging}.')
+async def send_countdown():
+    await bot.wait_until_ready()
+    channel = bot.get_channel(CHANNEL_ID)
+    if not channel:
+        print("Channel not found!")
+        return
 
-#DONE
-@bot.command(name='going', help='Specific person details needs <name> as parameter')
-@commands.cooldown(2,30,commands.BucketType.user)
-async def going_command(ctx, name:str):
-	for person in Group:
-		if name.lower() in person.name.lower():
-			going = "isn't going"
-			timeoff = "don't have time off"
-			flight = "don't have their flights booked"
-			lodging = "don't have a place to stay"
-			if person.timeoff:
-				timeoff = "have time off"
-			if person.flight:
-				flight = "have their flights booked"
-			if person.lodging:
-				lodging = "have a place to stay"
-			if person.going:
-				going = "is going"
-			await ctx.send(f'{person.name} {going}. They {timeoff}, {flight} and {lodging}.')
+    while not bot.is_closed():
+        now = datetime.datetime.now()
+        target_time = now.replace(hour=7, minute=0, second=0, microsecond=0)
+        if now > target_time:
+            target_time += datetime.timedelta(days=1)
+        wait_time = (target_time - now).total_seconds()
 
-#@bot.command(name='translate', help='Translate your text to japanese')
-#@commands.cooldown(3,30,commands.BucketType.user)
-#async def trans_command(ctx, words:str):
-#	translator = Translator(to_lang="ja")
-#	print(f'{words}')
-#	response = translator.translate(words) #words, src='en', dest='ja'
-#	print(response)
-#	await ctx.send(response.text)
+        await asyncio.sleep(wait_time)
+        today = datetime.date.today()
+        target_date = datetime.date(2026, 4, 1)
+        countdown_days = (target_date - today).days
 
-#DONE
-@bot.command(name='map', help='Get a map of our japan trip')
-@commands.cooldown(1,30,commands.BucketType.channel)
-async def map_command(ctx):
-	embed=discord.Embed(title="Our Japan Map",colour = discord.Colour.orange(), timestamp=ctx.message.created_at, url="https://www.google.com/maps/d/edit?mid=1o_mgr_3GXP_xbLO9mb4QeON7vLoxOSU&ll=35.13006297296019%2C138.11491213319218&z=8")
-	embed.set_image(url=(f"https://image.thum.io/get/width/1920/crop/1200/viewportWidth/2400/maxAge/1/noanimate/https://www.google.com/maps/d/edit?mid=1o_mgr_3GXP_xbLO9mb4QeON7vLoxOSU&ll=34.545604858227236%2C136.10263576493375&z=8"))
-	await ctx.send(embed=embed)
-
-#DONE
-@tasks.loop(hours=1.0)
-async def send_daily_message():
-	now = datetime.datetime.today()
-	mystart = now.replace(hour=7, minute=40, second=0)
-	print(f"Hour: {now.hour}. Wanted Hour: {mystart.hour}.")
-	if now.hour == mystart.hour:
-		channelToSend = await bot.fetch_channel(CHANNEL)
-		today = datetime.date.today()
-		future = datetime.date(2023,10,14)
-		diff = future - today
-		await channelToSend.send(f'<@&979099587560218644> Japan Countdown {diff.days} days remaining')
-
-@bot.event
-async def on_command_error(ctx, error):
-	if isinstance(error, commands.errors.CheckFailure):
-		await ctx.send('You do not have the correct role for this command.')
-	if isinstance(error, commands.CommandOnCooldown):
-		msg = '**Still on cooldown**, please try again in {:.2f}s'.format(error.retry_after)
-		await ctx.send(msg)
-
-@bot.command(name='test', help='test function')
-@commands.cooldown(1,60,commands.BucketType.channel)
-async def map_command(ctx):
-	await ctx.send('<@&979099587560218644> you are all poo faces')
+        await channel.send(f"<@&979099587560218644> Countdown to Japan 2026: {countdown_days} days remaining!\n{get_yen()}")
+        await asyncio.sleep(86400)  # Wait 24 hours before sending again
 
 @bot.event
 async def on_ready():
-	print(f"Bot Started")
-	send_daily_message.start()
-	print(f"loop Started")
+    print(f'Logged in as {bot.user}')
+    bot.loop.create_task(send_countdown())
 
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    await bot.process_commands(message)
+
+@bot.command(name="yen", help="Get the current exchange rate for Japanese Yen against GBP.")
+@cooldown(1, 60, BucketType.guild)
+async def yen(ctx):
+    await ctx.send(get_yen())
+
+@bot.command(name="plan", help="Retrieve and display a table from a Google Sheet.")
+@cooldown(1, 60, BucketType.user)
+async def plan(ctx):
+    sheet = get_google_sheet_data()
+    data = sheet.get_all_values()
+    if not data:
+        await ctx.send("Error retrieving the plan from Google Sheets.")
+        return
+
+    await ctx.send(f"Here is the plan:\n{data}")
+
+@bot.command(name="day", help="See what everyone is doing on a certain day of the holiday")
+@cooldown(1, 1, BucketType.user)
+async def day(ctx, *, message: str):
+    sheet = get_google_sheet_data()
+    people_data = {
+        "Lewis" : sheet.col_values(3),
+        "Bew"   : sheet.col_values(5),
+        "Ruan"  : sheet.col_values(7),
+        "Jack"  : sheet.col_values(9),
+        "Tom"   : sheet.col_values(11),
+        "Ethan" : sheet.col_values(13),
+        "Beth"  : sheet.col_values(15),
+        "Adrian": sheet.col_values(17),
+        "Neb"   : sheet.col_values(19)
+    }
+    try:
+        location_map = {}
+        day_int = int(message)
+        dayRow = sheet.row_values(day_int+2)
+        tableString = ""
+        for person, locations in people_data.items():
+            if (day_int +2)  < len(locations):
+                location = locations[(day_int +2)]
+                if location not in location_map:
+                    location_map[location] = []
+                location_map[location].append(person)
+        if "" in location_map:
+            location_map["Undecided"] = location_map.pop("")
+        tableString += f"{dayRow[1]}\n"+"\n".join([f"{location}: {', '.join(people)}" for location, people in location_map.items()])
+        await ctx.send(tableString)
+
+    except ValueError as ex:
+         await ctx.send(f"Can't understand that! Please give me an integer of the day you want to see.")
+
+print('i am starting!')
 bot.run(TOKEN)
